e5f2684437c922cdf8b7418e85d26a006c6d2772
holdendip <holdendip@gmail.com>
2025-10-08 07:44:13 -0400

    javascript changes oct 7

---
+++ js/resize.js
@@ -0,0 +1,81 @@
+
+
+//const image = document.getElementsByClassName('imagegrow');
+//
+//window.addEventListener('scroll', () => {
+//	if (window.scrollY > 100){
+//		image.classlist.add("resizing");
+//	} else{
+//		image.classList.remove("resizing");
+//	}
+//})
+//
+
+
+
+
+
+
+//function isInViewport(element){
+//	const rect = element.getBoundingClientRect();
+//	return rect.top <= window.innerHeight && rect.bottom >= 0;
+//}
+//
+//function handleScroll(){
+//	const elements = document.querySelectorAll('imagegrow');
+//	elements.forEach((el) => {
+//		if (isInViewport (el)){
+//			el.classList.add("resize");
+//		} else{
+//			el.classList.remove("resize");
+//		}
+//	});
+//}
+//
+//window.addEventListener('scroll', handleScroll);
+//window.addEventListener('load', handleScroll);
+
+
+
+
+//
+//$(function(){
+//	$(window).scroll(function(){
+//		if($(this).scrollTop() > 100){
+//			$('style_box').addClass("resize");
+//		}
+//		else{
+//			$('style_box').removeClass("resize");
+//		}
+//	});
+//});
+	
+
+
+
+
+
+
+
+
+
+//lets the box be resized by mouse click
+const resizable = document.getElementById('imagegrow');
+
+let isResizing = false;
+
+resizable.addEventListener('mousedown', () => {
+  isResizing = true;
+});
+
+window.addEventListener('mousemove', (e) => {
+  if (!isResizing) return;
+  const width = e.clientX - resizable.offsetLeft;
+  const height = e.clientY - resizable.offsetTop;
+  resizable.style.width = width + 'px';
+  resizable.style.height = height + 'px';
+});
+
+window.addEventListener('mouseup', () => {
+  isResizing = false;
+});
