b4c75546cc77d0d15c1f0f03a5e7a4d25f49f207
holdendip <holdendip@gmail.com>
2025-10-20 07:09:35 -0400

    most recent update

--- js/fade.js
+++ js/fade.js
@@ -1,22 +1,22 @@
 
 
-//function isInViewport(element){
-//	const rect = element.getBoundingClientRect();
-//	return rect.top <= window.innerHeight && rect.bottom >= 0;
-//}
-//
-//
-//function handleScroll(){
-//	const elements = document.querySelectorAll('.hidden');
-//	elements.forEach((el) => {
-//		if (isInViewport (el)){
-//			el.classList.add("show");
-//		}
-//	});
-//}
-//
-//window.addEventListener('scroll', handleScroll);
-//window.addEventListener('load', handleScroll);
+function isInViewport(element){
+	const rect = element.getBoundingClientRect();
+	return rect.top <= window.innerHeight && rect.bottom >= 0;
+}
+
+
+function handleScroll(){
+	const elements = document.querySelectorAll('.hidden');
+	elements.forEach((el) => {
+		if (isInViewport (el)){
+			el.classList.add("show");
+		}
+	});
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
 
 
 
